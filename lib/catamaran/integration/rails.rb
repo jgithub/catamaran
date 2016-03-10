88b50c20809ee781d19350428f6083a6599abae7
jgithub <jgithub1@jeano.net>
2016-03-10 16:25:41 -0600

    Update the path to the catamaran.rb config file for rails.

--- lib/catamaran/integration/rails.rb
+++ lib/catamaran/integration/rails.rb
@@ -7,7 +7,7 @@ module Catamaran
       if defined?( Rails::Railtie )
         class Railtie < Rails::Railtie
           initializer :load_base_catamaran do
-            initializer = Rails.root.join( "config", "initializers", "catamaran.rb" )
+            initializer = Rails.root.join( "config", "catamaran.rb" )
             require initializer if File.exist?( initializer )
 
             Catamaran::Manager.add_output_file(  Catamaran::OutputFile.new( Rails.root.join( "log", "#{Rails.env}.log" ) ) )
